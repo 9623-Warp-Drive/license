68058f4358c64224fe26b174a08f20711b4246ef
Z5483 <Z5483Y@gmail.com>
2020-07-19 22:50:12 -0400

    Refine license.sh

--- license.sh
+++ license.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Add license to current directory
 #
@@ -14,22 +14,22 @@ else
 fi
 
 REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
-LICENSE=$(ls $REPO/list 2> /dev/null | $FUZZY --multi --color=16 --preview="cat $REPO/list/{}" -q "$*")
-LICENSE_ARRAY=($(echo $LICENSE | tr " " "\n"))
+LICENSE=$(ls "$REPO/list" 2> /dev/null | $FUZZY --multi --color=16 --preview="cat $REPO/list/{}" -q "$*")
+LICENSE_ARRAY=($(echo "$LICENSE" | tr " " "\n"))
 
 main() {
-	if [[ ! -z $LICENSE ]]; then
+	if [[ -n "$LICENSE" ]]; then
 		printf 'Enter copyright year: '
-		read year
+		read -r year
 		printf 'Enter name of copyright owner: '
-		read name
+		read -r name
 
 		if [[ ${#LICENSE_ARRAY[@]} -eq 1 ]]; then
-			sed "s/\[year\]/$year/g;s/\[fullname\]/$name/g" $REPO/list/$LICENSE > ./LICENSE
+			sed "s/\[year\]/$year/g;s/\[fullname\]/$name/g" "$REPO/list/$LICENSE" > ./LICENSE
 		elif [[ ${#LICENSE_ARRAY[@]} -gt 1 ]]; then
 			mkdir ./LICENSE
 			for license in "${LICENSE_ARRAY[@]}"; do
-				sed "s/\[year\]/$year/g;s/\[fullname\]/$name/g" $REPO/list/$license > ./LICENSE/$license
+				sed "s/\[year\]/$year/g;s/\[fullname\]/$name/g" "$REPO/list/$license" > ./LICENSE/"$license"
 			done
 		fi
 
